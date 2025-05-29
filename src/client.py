03797bbd97dc3743b0721126b975e8b270b229c4
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-05-29 16:51:53 +0300

    core: Updated client, Added data file

--- src/client.py
+++ src/client.py
@@ -1,7 +1,15 @@
+import os
 import asyncio
 
 
-async def tcp_echo_client(message):
+async def main():
+    url = "http://127.0.0.1:8000"
+
+    message = "GET {}\r\nHost: {}\r\n".format(
+        f"http://127.0.0.1:8888/get?url={url}",
+        "client",
+    )
+
     reader, writer = await asyncio.open_connection(
         '127.0.0.1',
         8888
@@ -11,7 +19,14 @@ async def tcp_echo_client(message):
     writer.write(message.encode())
     await writer.drain()
 
-    data = await reader.read(100)
+    filepath = os.path.join("data", "server_content.html")
+    with open(filepath, "wb") as ftwb:
+        while True:
+            data = await reader.read(1000)
+            if not data:
+                break
+            ftwb.write(data)
+
     print(f'Received: {data.decode()!r}')
 
     print('Close the connection')
@@ -19,4 +34,5 @@ async def tcp_echo_client(message):
     await writer.wait_closed()
 
 
-asyncio.run(tcp_echo_client('Hello World!'))
+if __name__ == "__main__":
+    asyncio.run(main())
